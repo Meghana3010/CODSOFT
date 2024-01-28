e1462995078ce3ba41fdf490cc5e553aea1bbb6d
Meghana3010 <142252437+Meghana3010@users.noreply.github.com>
2024-01-28 23:15:35 +0530

    CurrencyConverter

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,11 +1,16 @@
+import java.util.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.io.BufferedReader;
+import java.text.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class CurrencyConverter {
     private static final String API_KEY = "";//Your API_KEY
-    private static final String API_URL = "https://api.currency.com/exchangerates/latest", private static final String API_URL = "https://v6.exchangerate-api.com/v6/"+API_KEY+"/latest/";
+    private static final String API_URL = "https://v6.exchangerate-api.com/v6/"+API_KEY+"/latest/";
     public static void main(String[] args) throws ParseException {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -14,9 +19,9 @@ public class CurrencyConverter {
             System.out.print("Enter the target currency (e.g., EUR): ");
             String targetCurrency = reader.readLine();
             System.out.print("Enter the amount to convert: ");
-            double amount = Double.parseDouble(reader.readLine())
+            double amount = Double.parseDouble(reader.readLine());
             double conversionResult = convertCurrency(baseCurrency, targetCurrency, amount);
-            System.out.println(amount + + baseCurrency" is equal to " + conversionResult + " " + targetCurrency);
+            System.out.println(amount +" " + baseCurrency+" is equal to " + conversionResult + " " + targetCurrency);
             reader.close();
         }catch (IOException e) {
             e.printStackTrace();
@@ -25,7 +30,6 @@ public class CurrencyConverter {
 
     private static double convertCurrency(String baseCurrency, String targetCurrency, double amount) throws IOException, ParseException {
         String url = API_URL+baseCurrency;
-        String url API_URL;
         URL apiURL = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) apiURL.openConnection();
         connection.setRequestMethod("GET");
@@ -50,11 +54,11 @@ public class CurrencyConverter {
                 }
             }else{
                 System.out.println("Rates not found in the API response.");
-            }else{ 
-                System.out.println("Failed to fetch data from the API. Response code: " + responseCode);
             }
-            return 0.0; 
+        }else{ 
+            System.out.println("Failed to fetch data from the API. Response code: " + responseCode);
         }
+        return 0.0; 
     }
 }
  
